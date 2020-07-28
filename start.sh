3051c50bf8dcbfc3b30f6b4efcac31e57f18e7ef
TomokiPC-Parent\tomokis <ts32worker@gmail.com>
2020-07-29 00:28:36 +0900

    シェルスクリプト修正

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 PORTAINER_DATA_DIR=/var/local/portainer/data
 
-if [ ! -f $PORTAINER_DATA_DIR ]; then
+if [ ! -d $PORTAINER_DATA_DIR ]; then
   mkdir -p $PORTAINER_DATA_DIR
 fi
 
@@ -11,8 +11,8 @@ docker service rm portainer
 docker service create \
   --name portainer \
   --replicas 1 \
-  --publish mode=host,target=8000,published=8000 \
-  --publish mode=host,target=9000,published=9000 \
+  --publish mode=host,target=8000,published=$1 \
+  --publish mode=host,target=9000,published=$2 \
   --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
   --mount type=bind,src=$PORTAINER_DATA_DIR,dst=/data \
   portainer/portainer
